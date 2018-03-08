9ad53cd3034db3b57a3e7f590f2f6cf0af8efef7
gy0p4k <gyorgypakozdi@gmail.com>
2018-03-08 11:44:32 +0100

    feat(login): hash in table

--- login.py
+++ login.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import dataset
+import hashlib
 
 app = Flask(__name__)
 db = dataset.connect('sqlite:///:memory:')
@@ -26,13 +27,19 @@ def bad_credentials():
 
 
 def validate(credentials):
-    return credentials["password"] == get_password(credentials["username"])
+    return get_hash(credentials["password"]) == get_password(credentials["username"])
 
 
 def get_password(name):
     user = table.find_one(username=name)
     return user["password"] if user else False
 
+
+def get_hash(key):
+    return hashlib.md5(key.encode('utf-8')).hexdigest()
+
+
 if __name__ == '__main__':
-    table.insert({"username": "admin", "password": "alma"})
+    table.insert(
+        {"username": "admin", "password": "ebbc3c26a34b609dc46f5c3378f96e08"})
     app.run()
